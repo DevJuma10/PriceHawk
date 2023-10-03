ad1bdf87adc31073194394fe28cb860db4cf074c
DevJuma10 <juma.study@gmail.com>
2023-10-03 12:04:13 +0300

    Feat: Poject scafold
    Setup dependencies, NextJs clean up and Add Navbar component

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import Navbar from '@/components/Navbar'
+import './globals.css'
+
+// FONTS
+import { Inter, Space_Grotesk } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+const spaceGrotesk = Space_Grotesk({
+    subsets:['latin'],
+  weight: ['300', '400', '500', '600', '700'] })
+
+export const metadata = {
+  title: 'PriceHawk',
+  description: 'Track product prices effortlessly and save money shoping online',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className='max-w-10xl'>
+          <Navbar />
+          {children}
+        </main>
+
+        
+      </body>
+    </html>
+  )
+}
